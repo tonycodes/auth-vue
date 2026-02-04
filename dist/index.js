fba5fb6eeeabf219b11a2b43d623790b52b5f4ca
Tony James <me@tony.codes>
2026-02-04 22:00:16 +0000

    Export AUTH_CONFIG_KEY and AUTH_INJECTION_KEY from barrel

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-export { AuthProvider, createAuthPlugin } from './AuthProvider.js';
+export { AuthProvider, createAuthPlugin, AUTH_CONFIG_KEY, AUTH_INJECTION_KEY } from './AuthProvider.js';
 export { useAuth } from './useAuth.js';
 export { SignedIn, SignedOut, RedirectToSignIn } from './components.js';
 export { OrganizationSwitcher } from './OrganizationSwitcher.js';
